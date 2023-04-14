ad5c72aeba2a4eb8f28c6bcbc66ca84d3814c1a6
ShivaniYS <87171146+ShivaniYS@users.noreply.github.com>
2023-04-14 13:11:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,190 +1,197 @@
-import { useState } from "react";
+import { useState } from 'react';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.css";
-import './App.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';  
-import 'bootstrap-css-only/css/bootstrap.min.css';  
-import 'mdbreact/dist/css/mdb.css';
-import { formik, useFormik } from "formik";
-import NumberFormat, { NumericFormat } from 'react-number-format';
-import { MDBContainer } from "mdbreact";
-import { Pie } from 'react-chartjs-2';
-import {Chart, ArcElement} from 'chart.js'
-Chart.register(ArcElement);
-function App() {
-  
-  const [formFields, setFormFields] = useState([
-    { name: '', CTC: '' },
-    { name: '', CTC: '' },
-    { name: '', CTC: '' },
-    { name: '', CTC: '' },
-    { name: '', CTC: '' },
-  ]);
-  const [val, setVal] = useState('');
-  const [percentage,setpercentage] = useState('');
-  const [count, setCount] = useState('');
-  const [esop, setEsop] = useState(false);
-  const [chartData, setChartData] = useState({
-    labels: [],
-    datasets: [
-      {
-        label: '',
-        data: [],
-        backgroundColor: [],
-      },
-    ],
-  });
-  const handleFormChange = (event, index) => {
-    let data = [...formFields];
-    data[index][event.target.name] = parseInt(event.target.value.replace(/,/g,''));
-    
-    setFormFields(data);
-  };
+import Select from "react-select";
+import data from './data.json';
 
-  
-  const handleFormChangeName = (event, index) => {
-    let data = [...formFields];
-    data[index][event.target.name] = event.target.value;
-    setFormFields(data);
-  };
+Number.prototype.format = function () {
+  return this.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+};
+
+const EditableTable = () => {
+  const [employeeData, setEmployeeData] = useState(data);
+  const [count, setInputCount] = useState(1);
+  const [val, setVal] = useState(1000000);
+  const [total, setTotal] = useState(4.9);
+  const[POOL,setPool] = useState(10);
+  const options1 = [
+    { value: "Seed", label: "Seed" },
+    { value: "Series A", label: "A" },
+    { value: "Series B", label: " B" },
+    { value: "Series C", label: "C" },
+  ];
+  const Options = [
+    { value: "Senior", label: "Senior" },
+    { value: "Midlevel", label: "Midlevel" },
+    { value: "Junior", label: "Junior" },
+  ];
+
 
  
-  const submit = (e) => {
-    e.preventDefault();
-  };
 
-  const addFields = () => {
-    const arr = [];
-    for (let cnt = 0; cnt < +count; cnt++) {
-      arr.push({
-       name: '',
-       CTC: '',
-      });
-    }
-    setFormFields([...formFields, ...arr]);
-  };
-
-  const removeFields = (index) => {
-    let data = [...formFields];
-    data.splice(index, 1);
-    setFormFields(data);
-  };
-  let total = 0;
   
+     
+    
+  const onChangeInput = (e, employeeID) => {
+    let { name, value } = e.target;
+    let obj;
+    if (name === 'salary') {
+      value = value.split(',').join('');
+      if (isNaN(value)) {
+        e.target.value = '';
+        return;
+      }
+      obj = {
+        [name]: +value,
+        
+          ownership: (+value / +val) * 100,
+        
+        
+      };
+      const prevOwnership = employeeData.filter(
+        (item) => item.employeeID === employeeID
+      )[0].ownership;
+      const newOwnership = (+value / +val) * 100;
+      setTotal(total + (newOwnership - prevOwnership));
+    } else {
+      obj = { [name]: value };
+    }
+    const editData = employeeData.map((item) => {
+      return item.employeeID === employeeID && name
+        ? { ...item, ...obj }
+        : item;
+       
+    });
+
+    setEmployeeData(editData);
+  };
+
+  const handleClick = () => {
+    const appendedData = [];
+    for (let index = 0; index < count; index++) {
+      const element = {
+        employeeID: employeeData.length + index + 1,
+        role: 'New Hire',
+        salary: 5000,
+        ownership: (5000 / +val) * 100,
+      };
+      appendedData.push(element);
+    }
+    setEmployeeData([...employeeData, ...appendedData]);
+    setTotal(total + (5000 / +val) * 100 * count);
+  };
   
   return (
-    
-    <div className='App' >
-    
-    <div className='header'>ESOP POOL Calculator</div>
-    <div className='global'>
-    
-    
-      <div class="settings">
-      <label><h3 className='heading1'>Enter Company Valuation</h3></label>
-      <NumericFormat className='valuation'
-      thousandSeparator = {true}
-        type='text'
-        name= 'val'
-        
-        value={val}
-        onChange={(event) => setVal(event.target.value)}
-        
-        placeholder = "Enter Company's Valuation... "
-        />
-        
-      </div>
-      <div class="settings">
-      <label><h3 className='heading2'>Enter ESOP POOL Percentage</h3></label>
-      <NumericFormat className='percentage'
-        type='ESOP_Percentage'
-        name= 'percentage'
-        
-        value={percentage}
-        onChange={(event) => setpercentage(event.target.value)}
-        
-        placeholder = "Enter ESOP POOL % "
-        />
-        
-      </div>
+    <div className='container'>
       
-      <h3 className='heading'>Enter Role and CTC offered to an Employee...</h3>
-      <div  class="grid-container">
-      <div class="grid-child magenta">
-      <form onSubmit={submit}>
-        {formFields.map((form,index) => {
-          return (
-            <div  key={index}>
-              <input className='Name'
-                name='name'
-                placeholder='Role'
-                onChange={(event) => handleFormChangeName(event, index)}
-                value={form.name}
-              />
-              
-             
-              <NumericFormat className='ctc'
-              thousandSeparator = {true}
-                name='CTC'
-                placeholder='CTC'
-                onChange={(event) => handleFormChange(event, index)}
-                value={form.CTC}
-              />
-              <button className = 'button-72' role="button" onClick={() => removeFields(index)}>Remove</button>
-              </div>
-          );
+      <h1 className='title'>Employee Options Pool</h1>
+      <tr>
+      <th><label for={'val'}>Enter Valuation: </label>
+      <input
+        style={{ width: 'fit-content' }}
+        name='val'
+        type='text'
+        value={val.format()}
+        onChange={(e) => {
+          const newVal = Number(e.target.value.split(',').join(''));
+          if (isNaN(newVal)) {
+            e.target.value = '';
+            return;
+          }
           
-        })}
-        
-            
-      </form>
-      </div>
 
-       <div class = "grid-child1 magenta">
-       <h3 className="heading3">Percentage of ESOPs </h3>
-        <div className='section'>{formFields.map(({ name, CTC }) => {
-        console.log(total);
-        let finval = parseInt(val.replace(/,/g,''))
-        let perc = CTC/finval;
-        //let perc = (+CTC / +val) * 100;
-        total += perc;
-        return <p>{`${name}: ${perc}%`}</p>;
-      })}</div>
-      </div>
-      </div>
-      
-      <input className='Addmore'
-        type='text'
-        name='count'
-        value={count}
-        onChange={(event) => setCount(event.target.value)}
-        placeholder='Enter Count...'
+          let newTotal = 0;
+          const editData = employeeData.map((item) => {
+            newTotal += (item.salary / newVal) * 100;
+            return { ...item, ownership: (item.salary / newVal) * 100 };
+          });
+          setTotal(newTotal);
+          setEmployeeData(editData);
+          setVal(newVal);
+        }}
+        placeholder='Type Valuation'
       />
-
-      <button className='button-72' onClick={addFields}>Add More..</button>
-     
-      <br />
-      <button className='button-72-submit' onClick={submit}>Submit</button>
-      </div>
+      </th>
+      <th>
+      <label for = {'POOL'}>Enter ESOP POOL  :</label>
+      <input
+                  name='POOL'
+                  type='text'
+                  value={`${POOL}%`}
+                  // onChange={(e) => onChangeInput(e, employeeID)}
+                  placeholder='Type Pool %'
+                /></th>
+      <th>
+      <div className="mt-5 m-auto w-30">
+      Funding Stage: <Select options={options1} />
+       </div>
+       </th>
+       </tr>
       
 
-      
 
-      
-      {total ? (
-        <h3 className='footer'>ESOP Pool Size: {total}</h3>
-      ) : (
-        <h3 className='footer'>Please Fill Above Fields</h3>
-        
-      )}
-      
-      
-      
-      
-        
+      <table>
+        <thead>
+          <tr>
+          <th>Seniority </th>
+            <th>Role</th>
+            <th>CTC Offered</th>
+            <th>Ownership</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          {employeeData.map(({ employeeID, role, salary, ownership }) => (
+            <tr key={employeeID}>
+            <td> <Select options={ Options} /></td>
+              <td>
+                <input
+                  name='role'
+                  value={role}
+                  type='text'
+                  onChange={(e) => onChangeInput(e, employeeID)}
+                  placeholder='Type role'
+                />
+              </td>
+              <td>
+                <input
+                  name='salary'
+                  value={salary.format()}
+                  type='text'
+                  onChange={(e) => onChangeInput(e, employeeID)}
+                  placeholder='Type salary'
+                />
+              </td>
+              <td>
+                <input
+                  name='ownership'
+                  type='text'
+                  value={`${ownership}%`}
+                  // onChange={(e) => onChangeInput(e, employeeID)}
+                  placeholder='Type ownership'
+                />
+              </td>
+              {/* <img
+                width={'15em'}
+                src='https://cdn-icons-png.flaticon.com/512/2907/2907762.png'
+              /> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p>Total: {total}%</p>
+      <input
+        style={{ width: '7rem' }}
+        name='count'
+        type='text'
+        value={count}
+        onChange={(e) => {
+          setInputCount(e.target.value);
+        }}
+        placeholder='Type Count'
+      />
+      <button onClick={handleClick}>Add More</button>
     </div>
   );
-       
-}
+};
 
-export default App;
+export default EditableTable;
